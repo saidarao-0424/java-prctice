d944d07e55b86b35e14cf4f9c324cfd7950af7bf
saidarao <saidarao.kotari@gmail.com>
2022-06-07 01:06:16 +0800

    invoke mul from main method

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld {
 
     public static void main (String args[]){
         System.out.println("HELLO I want addtion"+add(10,20));
-        System.out.println("HELLO I want addtion"+sub(30,20));
+        System.out.println("HELLO I want sub"+sub(30,20));
+        System.out.println("HELLO I want mul"+mul(30,20));
     }
 
     public static int add(int num1, int num2){
