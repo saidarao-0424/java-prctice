a22e835d73cbec0710b63e9457caedbb87ddaea6
saidarao <saidarao.kotari@gmail.com>
2022-06-07 00:55:44 +0800

    Added add method for calculating sum

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld {
 
     public static void main (String args[]){
-        System.out.println("HELLO World");
+        System.out.println("HELLO I want addtion"+add(10,20));
+    }
+
+    public static int add(int num1, int num2){
+        return num1+num2;
     }
 }
